3fec7c6988325902664253024f707b2b91f2c52b
Felix Stollenwerk <felix.stollenwerk@ai.se>
2022-07-18 12:05:14 +0200

    refactoring

--- convert_gpt/hf_play.py
+++ convert_gpt/hf_play.py
@@ -2,13 +2,14 @@ from transformers import GPT2LMHeadModel, AutoTokenizer, pipeline
 # import tokenizers
 import sentencepiece as spm
 import torch
+from typing import Tuple, List
 
 
 PATH_MODEL_DIR = "/home/joey/code/gpt_convert/Megatron-LM/HF_model_converted"
 TOKENIZER_PATH = PATH_MODEL_DIR + "/model.model"
 
 
-def load_model_and_tokenizer():
+def load_model_and_tokenizer() -> Tuple[GPT2LMHeadModel, spm.SentencePieceProcessor]:
     # EDIT THESE PATHS
     model = GPT2LMHeadModel.from_pretrained(PATH_MODEL_DIR)
     model.eval()
@@ -20,35 +21,41 @@ def load_model_and_tokenizer():
     return model, sp_tokenizer
 
 
-def encode_text(sp_tokenizer, text):
-    tokenized_text = sp_tokenizer.encode(text)
-
-    tokenized_text = torch.tensor(tokenized_text, dtype=torch.long)
-    tokenized_text.unsqueeze_(0)
-
-    return tokenized_text
+def encode_text(sp_tokenizer: spm.SentencePieceProcessor, text: str) -> List[int]:
+    token_ids = sp_tokenizer.encode(text)
+    return token_ids
 
 
-def generate(sp_tokenizer, model, text):
-    tokenized_text = encode_text(sp_tokenizer, text)
-
-    # gen = base_model.generate(tokenized_text, repetition_penalty=2.)
-    gen = model.generate(tokenized_text)
-
-    list_gen = gen[0].numpy().tolist()
-
-    decoded = sp_tokenizer.decode(list_gen)
-    return decoded
+def decode_token_ids(sp_tokenizer: spm.SentencePieceProcessor, token_ids: List[int]) -> str:
+    text = sp_tokenizer.decode(token_ids)
+    return text
 
 
-def main():
+def generate(model: GPT2LMHeadModel, input_token_ids: List[int]) -> List[int]:
+
+    input_token_ids = torch.tensor(input_token_ids, dtype=torch.long)
+    input_token_ids.unsqueeze_(0)
+
+    # generated_output = base_model.generate(input_token_ids, repetition_penalty=2.)
+    generated_output = model.generate(input_token_ids)
+    generated_token_ids = generated_output[0].numpy().tolist()
+
+    return generated_token_ids
+
+
+def main(input_texts):
     model, sp_tokenizer = load_model_and_tokenizer()
 
-    text = "Hej jag heter Ariel"
+    for input_text in input_texts:
+        input_token_ids = encode_text(sp_tokenizer, input_text)
+        generated_token_ids = generate(model, input_token_ids)  # generated_token_ids includes input_token_ids
+        generated_text = decode_token_ids(sp_tokenizer, generated_token_ids)
 
-    generated_text = generate(sp_tokenizer, model, text)
-    print(generated_text)
+        print(generated_text)
 
 
 if __name__ == '__main__':
-    main()
+    _input_texts = [
+        "Hej jag heter Ariel",
+    ]
+    main(_input_texts)
